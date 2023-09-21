61e3bc52f28ace4b2c1516612c3cc681567a0d4e
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-21 17:51:48 +0900

    Add user model

---
+++ Cupcake Corner/Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Анна Перехрест  on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
